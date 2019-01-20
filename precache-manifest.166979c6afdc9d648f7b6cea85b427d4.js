6ac08c31355f663d0ef81894954edb4df34eb2e7
unrealisher <unrealmpa3@mail.ru>
2019-01-20 15:40:48 +0300

    Updates

---
+++ precache-manifest.166979c6afdc9d648f7b6cea85b427d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "0a92fa0ae32c0778f9a9",
+    "url": "/todo/static/js/main.0a92fa0a.chunk.js"
+  },
+  {
+    "revision": "d84f417f025289fbf00c",
+    "url": "/todo/static/js/1.d84f417f.chunk.js"
+  },
+  {
+    "revision": "0a92fa0ae32c0778f9a9",
+    "url": "/todo/static/css/main.9c9b7b42.chunk.css"
+  },
+  {
+    "revision": "3a8ec655c83cfc7c8664caa60055f06f",
+    "url": "/todo/index.html"
+  }
+];
